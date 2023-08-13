bbe5cf9c90cf40a49136f9e0e1918ee72a120d55
cliffordbechtel <crbechtel@gmail.com>
2023-08-13 18:32:12 -0400

    validation exception handler

--- src/main/java/com/cliffordbechtel/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/cliffordbechtel/msscbeerservice/web/controller/BeerController.java
@@ -3,9 +3,11 @@ package com.cliffordbechtel.msscbeerservice.web.controller;
 import com.cliffordbechtel.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+
 
 @RequestMapping("/api/v1/beer")
 @RestController
@@ -19,14 +21,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
 
         // todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto) {
 
         // todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
